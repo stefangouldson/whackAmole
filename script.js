1c5e49ec4f7287617161a4476cffe94a631e481f
CN0103 <cn0103@CN103.local>
2019-09-04 16:28:08 +0100

    final+comments

--- script.js
+++ script.js
@@ -1,31 +1,28 @@
-let img="./mole.png"
-let place=document.getElementsByClassName("placeImg")
-
-let moleBtn=document.getElementById("example")
-
+let img="./mole.png"                                    //define mole img in js
+let place=document.getElementsByClassName("placeImg")   //creates array of mole positions
 
 //this function generates a random place for the mole
 const playFunc=()=>{
-for (i=0;i<12;i++){place[i].src="blank.png"} //makes all the squares blank
-let x=Math.floor(Math.random()*12)           //generates random number between 0-11
-molePlace=place[x]                           //define position for mole
-molePlace.src=img                            //put mole there
-
+for (i=0;i<12;i++){
+    place[i].src="blank.png"                        //makes every img blank
+    place[i].removeEventListener("click",addTotal)  //removes add total function from every position
+}  
+    let x=Math.floor(Math.random()*12)           //generates random number between 0-11
+    molePlace=place[x]                           //define position for mole
+    molePlace.src=img                            //put mole there
+    place[x].addEventListener("click",addTotal)  //add click to add to total
 }
 
 //function to run once button is clicked
 function thePlay(){
-    myVar=setInterval(playFunc,1000)
-    document.getElementById("play").disabled=true
+    myVar=setInterval(playFunc,1000)               //display mole every second
+    document.getElementById("play").disabled=true  //hide play button as it causes errors pressed twice
+    scoreplace.innerHTML=0                         //set score to 0
 }
 
-// let imgButton=document.createElement("Button")
-// imgButton.innerHTML="seriously, where are you?"
-// document.body.appendChild(imgButton)
-
-let scoreplace=document.getElementById("score")
-let total=0
-const addTotal=()=>{
-    total+=1
-    scoreplace.innerHTML=total
+let scoreplace=document.getElementById("score") //define score in js
+let total=0                                     //gloabal declare total
+const addTotal=()=>{                            //function called on click
+    total+=1                                    //add 1 to total
+    scoreplace.innerHTML=total                  //display total score
 }
